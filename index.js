2306cb24e9ac954f13f1145b89497392982d7322
Ryan Carlos <ryancarlos925@gmail.com>
2022-05-10 22:06:14 -0700

    commit tree 0eaac729a370f11305f5a47220b81cef9b854501

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+// function filter(collection,cb){
+//     const newDrivers = [];
+//     for(const user of collection) {
+//         if(cb(user)) {
+//             newDrivers.push(user);
+//         }
+//     }
+// }
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(word => word.length > 6);
+
+// console.log(result);
+
+function findMatching(array, string) {
+    const result = array.filter(match => match.toLowerCase() === string.toLowerCase());
+    return result
+}
+
+function fuzzyMatch(array,string) {
+    const result = array.filter(match => match.charAt(0) === string.charAt(0));
+    return result
+}
+
+function matchName(array,string) {
+    const result = array.filter(match => match.name === string)
+    return result
+}
